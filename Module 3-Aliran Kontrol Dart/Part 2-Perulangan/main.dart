582ecdb1ff11c723af30b83f5e8c6a9acd0c7576
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-11-02 14:28:01 +0700

    Initian commit

---
+++ Module 3-Aliran Kontrol Dart/Part 2-Perulangan/main.dart
@@ -0,0 +1,24 @@
+//PERULANGAN atau bisa juga disebut looping (LOOP)
+//Sintaks : for(nilai awal; kondisi pembanding; langkah){}
+
+//Contoh Case
+void main() {
+  // for (int i = 0; i < 5; i++) {
+  //   print('Hello World');
+  // }
+
+  // BISA JUGA MENGGUNAKAN SINTAKS LAIN, JIKA DATA NYA BERBENTUK LARIK/ARRAY
+  // var empatSekawan = ['Nathan', 'Kevin', 'Arman', 'Udin'];
+  // for (var anggota in empatSekawan) {
+  //   print(anggota);
+  // }
+
+  // var ngulang = 5;
+  // for (int x = 1; x < ngulang; x++) {
+  //   print('x');
+  // }
+
+  for(int i = 0; i < 5; i++){
+    print('5');
+  }
+}
