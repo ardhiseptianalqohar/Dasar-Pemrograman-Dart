679556cdf050ff9a9c419040e36dbdb06a21863f
Ardhi Septian Al qohar <110322746+ardhiseptianalqohar@users.noreply.github.com>
2022-11-13 12:31:07 +0700

    Update main.dart

--- Module 2-Dasar Dart/Part 4-Fungsi/main.dart
+++ Module 2-Dasar Dart/Part 4-Fungsi/main.dart
@@ -7,3 +7,19 @@ void cetakTambah(int angka1, int angka2) {
 void main() {
   cetakTambah(1, 2);
 }
+
+// MENGHITUNG LUAS SEGITIGA
+
+
+// void cetakTambah(int alas, int tinggi) {
+//   double alas = 20;
+//   double tinggi = 15;
+//   double luas;
+//     luas = (alas * tinggi) / 2;
+//   print(alas * tinggi / 2);
+// }
+
+// void main(){
+//   cetakTambah(20, 15);
+// }
+
